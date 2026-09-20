f8008fb4cdf305d559197265b99a4301caf13693
2410010284noviafitri <2410010284novia@gmail.com>
2026-09-20 16:57:46 +0800

    menambahkan class Kartu Mahasiswa

---
+++ p01-halo-java/src/main/java/id/ac/uniska/pbo2/p01/KartuMahasiswa.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package id.ac.uniska.pbo2.p01;
+
+/**
+ *
+ * @author user
+ */
+public class KartuMahasiswa {
+    
+    public static void main(String [] args) {
+    String nama = "Novia Fitri";
+    String prodi = "Teknik Informatika";
+    String npm = "2410010284";
+    int semester = 5;
+    
+        System.out.println("====================================");
+        System.out.println("        KARTU MAHASISWA PBO 2       ");
+        System.out.println("====================================");
+        System.out.println("Nama        : " + nama );
+        System.out.println("NPM         : " + npm );
+        System.out.println("Prodi       : " + prodi);
+        System.out.println("Semester    : " + semester);
+        System.out.println("=====================================");
+        
+    }    
+}
